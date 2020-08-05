f3bb4eb3c7595a56835e64facc9e9a6dec97060a
imogenkraak <imogen.kraak@gmail.com>
2020-08-05 17:07:48 +0100

    Created domain model

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -10,15 +10,5 @@ describe DockingStation do
     bike = station.release_bike
     expect(bike.working?).to eq true
   end
+
 end
-# step 10 test:
- #it 'Expects DockingStation.release_bike to get a Bike object' do
-  # bike = subject.release_bike
-  #  expect(bike).to be_working
-# end
-
-
-
-#Add a test to your DockingStation specification that
-#a) gets a bike, and then
-#b) expects the bike to be working
