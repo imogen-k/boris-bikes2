0b0dd5aa00eefc26e2919311d9ec8344107a1c5e
Zish Jawaid <zisheanj@gmail.com>
2020-08-05 17:51:00 +0100

    Ch 11 added docking unit test

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,13 +2,24 @@ require 'DockingStation'
 
 
 describe DockingStation do
-  # it 'Expects DockingStation to respond to release_bike' do
-  # expect(DockingStation.new).to respond_to(:release_bike)
-  it { is_expected.to respond_to :release_bike }
-  it 'bike working' do
-    station = DockingStation.new
-    bike = station.release_bike
-    expect(bike.working?).to eq true
+
+  #it { is_expected.to respond_to :release_bike }
+  it 'Expects DockingStation to respond to release_bike' do
+    expect(DockingStation.new).to respond_to(:release_bike)
   end
+  
+  it 'Expects bike to be working' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  it 'Expects docking to be possible' do
+    station = DockingStation.new
+    expect(station).to respond_to(:dock)
+
+  end
+
+  
+
 
 end
