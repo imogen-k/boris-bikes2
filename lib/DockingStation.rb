771db79a192a60e74ca464ac095f9dfdf3a7aa4e
imogenkraak <imogen.kraak@gmail.com>
2020-08-05 15:47:04 +0100

    Ch 10 unit test working? method

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,7 @@
-require 'Bike'
+#require_relative 'Bike'
 
 class DockingStation
   def release_bike
-    Bike.new
+
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,15 +1,22 @@
 require 'DockingStation'
 
+
 describe DockingStation do
   # it 'Expects DockingStation to respond to release_bike' do
   # expect(DockingStation.new).to respond_to(:release_bike)
   it { is_expected.to respond_to :release_bike }
-
-  #it 'Expects DockingStation.release_bike to get a Bike object' do
-  #  bike = subject.release_bike
-  #  expect(bike).to eq(true)
-#  end
+  it 'bike working' do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike.working?).to eq true
+  end
 end
+# step 10 test:
+ #it 'Expects DockingStation.release_bike to get a Bike object' do
+  # bike = subject.release_bike
+  #  expect(bike).to be_working
+# end
+
 
 
 #Add a test to your DockingStation specification that
